26a152f98412dcec06ab1d5d8c4e3599e1e75646
HaydenEStern <stern.hayden.e@gmail.com>
2017-11-29 18:59:35 -0800

    test database

--- config/connection.js
+++ config/connection.js
@@ -11,30 +11,18 @@ var config = {
 
 var pool = mysql.createPool(config);
 
-module.exports = {
-    query: function(){
-        var sql_args = [];
-        var args = [];
-        for(var i=0; i<arguments.length; i++){
-            args.push(arguments[i]);
-        }
-        var callback = args[args.length-1]; //last arg is callback
-        pool.getConnection(function(err, connection) {
-        if(err) {
-                console.log(err);
-                return callback(err);
-            }
-            if(args.length > 2){
-                sql_args = args[1];
-            }
-        connection.query(args[0], sql_args, function(err, results) {
-          connection.release(); // always put connection back in pool after last query
-          if(err){
-                    console.log(err);
-                    return callback(err);
-                }
-          callback(null, results);
-        });
-      });
-    }
-};
+pool.getConnection(function(err, connection) {
+  // Use the connection
+  connection.query('SELECT something FROM sometable', function (error, results, fields) {
+    // And done with the connection.
+    connection.release();
+
+    // Handle error after the release.
+    if (error) throw error;
+
+    // Don't use the connection here, it has been returned to the pool.
+  });
+});
+
+
+module.exports = pool;
